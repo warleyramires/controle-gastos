c223071bc8b3e4609c0390f26d11dfc187ba0502
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-04-04 16:21:57 -0300

    feat: first commit

---
+++ src/main/java/com/controlegastos/ControledeGastos/ControledeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.controlegastos.ControledeGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlegastos/ControledeGastos/ControledeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlegastos.ControledeGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
